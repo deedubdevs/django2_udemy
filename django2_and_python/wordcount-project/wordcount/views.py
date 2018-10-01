06d9cee33f1999aad5e328512894aa3ca2353b36
Dewald Laurens <dewaldlaurens@Dewald-MacBook-Pro.local>
2018-10-01 18:26:12 +0200

    Initial commit

---
+++ django2_and_python/wordcount-project/wordcount/views.py
@@ -0,0 +1,23 @@
+import operator
+
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def count(request):
+    fulltext = request.GET['fulltext']
+    wordlist = fulltext.split()
+    word_dictionary = {}
+
+    for word in wordlist:
+        if word in word_dictionary:
+            word_dictionary[word] += 1
+        else:
+            word_dictionary[word] = 1
+
+    sorted_words = sorted(word_dictionary.items(), key=operator.itemgetter(1), reverse=True)
+    context = {'fulltext': fulltext, 'count': len(wordlist), 'sorted_words': sorted_words}
+    return render(request, 'count.html', context)
