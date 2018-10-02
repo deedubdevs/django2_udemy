11c35e64059dcbc20950dc578623b686cb514723
Dewald Laurens <dewaldlaurens@Dewald-MacBook-Pro.local>
2018-10-02 18:11:08 +0200

    Added models for Blog app

--- django2_and_python/portfolio/blog/models.py
+++ django2_and_python/portfolio/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100, default="Blog title")
+    pub_date = models.DateTimeField(name="Date published", default=timezone.now)
+    body = models.TextField(blank=True)
+    image = models.ImageField(upload_to="images/", blank=True)
+
+    def __str__(self):
+        return self.title
